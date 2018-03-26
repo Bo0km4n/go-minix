626efd43bbd96db50e3fe15c5d9089ff7ca49b04
Bo0km4n <pokopenkatuya@gmail.com>
2018-03-26 18:25:47 +0900

    Refactoring

--- operations/mov.go
+++ operations/mov.go
@@ -36,14 +36,6 @@ func (mov *MOV) Analyze(ctx *Context, inst byte) (int, string) {
 		fromOrTo := d == 0x01
 		regStr := regFunc(reg)
 		return getModRegRM(ctx, mod, rm, fromOrTo, regStr, "mov", regFunc)
-	case 0xb0:
-	case 0xb1:
-	case 0xb2:
-	case 0xb3:
-	case 0xb4:
-	case 0xb5:
-	case 0xb6:
-	case 0xb7:
 	case 0xb8:
 		regCode := inst & maskLow3
 		reg := Reg16b(regCode)
@@ -63,10 +55,6 @@ func (mov *MOV) Analyze(ctx *Context, inst byte) (int, string) {
 		rw := Reg16b(reg)
 		iw := fmt.Sprintf("%02x%02x", ctx.Body[ctx.Idx+1], ctx.Body[ctx.Idx+2])
 		return 3, getResult(ctx.Idx, getOrgOpe(ctx.Body[ctx.Idx:ctx.Idx+3]), getOpeString("mov", rw, iw))
-	case 0xbc:
-	case 0xbd:
-	case 0xbe:
-	case 0xbf:
 	default:
 		return 0, ""
 	}
