5193c0f055782755edacbace12d9905f610071d3
Bo0km4n <pokopenkatuya@gmail.com>
2019-04-25 21:47:13 +0900

    Fix cmp instruction when 0x837e

--- operations/grp.go
+++ operations/grp.go
@@ -139,31 +139,16 @@ func (grp *GRP) matchOpe1WB(ctx *Context, inst byte, mode byte) (int, string) {
 		dataStr := fmt.Sprintf("%02x", data)
 
 		return 3, getResult(ctx.Idx, getOrgOpe(ctx.Body[ctx.Idx:ctx.Idx+3]), getOpeString("sub", ea, dataStr))
-	case 0x07:
-		s := inst & 0x02 >> 1
-		w := inst & 0x01
+	case 0x07: // CMP Immediate with Register/Memory s = 1, w = 1
 		opt := ctx.Body[ctx.Idx+1]
 		mod := opt & maskTop2 >> 6
 		rm := opt & maskLow3
-		data := ctx.Body[ctx.Idx+2]
+		disp := ctx.Body[ctx.Idx+2]
+		data := ctx.Body[ctx.Idx+3]
 
-		if s == 0x00 && w == 0x01 {
-			addtionalData := ctx.Body[ctx.Idx+3]
-			ea := ""
-			if mod == 0x01 {
-				disp := signExtend(data)
-				ea = getRM(mod, rm, int(int16(disp)))
-			} else {
-				ea = getRM(mod, rm, int(data))
-			}
-			dataStr := fmt.Sprintf("%d", signExtend(addtionalData))
-
-			return 4, getResult(ctx.Idx, getOrgOpe(ctx.Body[ctx.Idx:ctx.Idx+4]), getOpeString("cmp", ea, dataStr))
-		}
-		ea := getRM(mod, rm, int(data))
-		dataStr := fmt.Sprintf("%02x", data)
-
-		return 3, getResult(ctx.Idx, getOrgOpe(ctx.Body[ctx.Idx:ctx.Idx+3]), getOpeString("cmp", ea, dataStr))
+		ea := getRM(mod, rm, int(int16(signExtend(disp))))
+		dataExtended := fmt.Sprintf("%x", signExtend(data))
+		return 4, getResult(ctx.Idx, getOrgOpe(ctx.Body[ctx.Idx:ctx.Idx+4]), getOpeString("cmp", ea, dataExtended))
 
 	default:
 		return OVER_RANGE, ""
