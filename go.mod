811a282315fbad5712da3510fb8919f8e3ef6105
Bo0km4n <pokopenkatuya@gmail.com>
2019-05-29 21:08:46 +0900

    Add modules

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/Bo0km4n/go-minix
+
+require (
+	github.com/k0kubun/pp v3.0.1+incompatible
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/spf13/cobra v0.0.4
+)
--- kernel/pkg/core/run.go
+++ kernel/pkg/core/run.go
@@ -1,11 +1,15 @@
 package core
 
-import "github.com/Bo0km4n/go-minix/kernel/pkg/core/kernel"
+import (
+	"github.com/Bo0km4n/go-minix/kernel/pkg/core/kernel"
+	"github.com/k0kubun/pp"
+)
 
 func Run(filename string) error {
 	if err := loadBin(filename); err != nil {
 		return err
 	}
+	pp.Println([]byte(filename))
 	kernel.K.Run()
 	return nil
 }
